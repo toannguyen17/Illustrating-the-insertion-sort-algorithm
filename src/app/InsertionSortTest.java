c10c716380efbf0345ca5b7ae652c7d98cfcf56d
MrT98 <toan.vp98.wapmaster@gmail.com>
2020-05-15 14:29:15 +0700

    show sort

--- src/app/InsertionSortTest.java
+++ src/app/InsertionSortTest.java
@@ -19,37 +19,32 @@ public class InsertionSortTest {
 		for(int i = 0; i < list.length; i++){
 			int currentMap      = list[i];
 			int currentMapIndex = i;
-			for(int j = list.length-1; j >= 0; j--){
+			for(int j = i-1; j >= 0; j--){
 				if (list[j] > currentMap){
+					//System.out.println("\n" + list[j] + " " + currentMap);
+					System.out.println("\n");
 					list[currentMapIndex] = list[j];
 					list[j] = currentMap;
 					currentMapIndex = j;
+
+					for(int item: list){
+						if (item == currentMap)
+							System.out.print("[" + item + "] ");
+						else
+							System.out.print(item + " ");
+					}
+
 				}
 			}
 		}
 	}
 
 	public static void main(String[] args) {
-
 		int[] list = {99,98,78,4,5,8,3,2,94,45,81,100,9,71};
 
 		System.out.println("List");
-		for(int item: list){
-			System.out.print(item + " ");
-		}
 
 		InsertionSortASC(list);
 
-		System.out.println("\nASC");
-		for(int item: list){
-			System.out.print(item + " ");
-		}
-
-		InsertionSortDESC(list);
-
-		System.out.println("\nDESC");
-		for(int item: list){
-			System.out.print(item + " ");
-		}
 	}
 }
